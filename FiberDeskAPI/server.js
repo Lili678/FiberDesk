9d108e6019d97050635c0910d6c7ae09c0dfc179
Lilibeth <127077093+Lili678@users.noreply.github.com>
2025-12-05 23:10:02 -0600

    Proyecto Android + Backend

---
+++ FiberDeskAPI/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Conexión Mongo
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongo conectado correctamente"))
+  .catch(err => console.error("Error al conectar Mongo:", err));
+
+// Rutas
+app.use('/api', require('./routes'));
+
+// Iniciar servidor
+app.listen(3000, () => {
+  console.log("Servidor backend corriendo en puerto 3000");
+});

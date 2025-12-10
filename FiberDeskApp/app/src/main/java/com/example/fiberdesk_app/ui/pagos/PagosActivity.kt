961c0f59ef24fa86d69a1a105ff8d0d770a0bef6
afthon21 <arturotsantos2003@gmail.com>
2025-12-10 14:02:51 -0600

    cambios en las busquedas

---
+++ FiberDeskApp/app/src/main/java/com/example/fiberdesk_app/ui/pagos/PagosActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fiberdesk_app.ui.pagos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fiberdesk_app.R
+
+class PagosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard_pago)
+    }
+}

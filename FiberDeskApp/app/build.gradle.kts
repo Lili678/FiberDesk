8041982989036f1bb18416f9ec7005f130805886
Roberto Negrete <notlurk64@gmail.com>
2025-12-11 01:40:02 -0600

    Configuracion final del modulo pare agregar tickets

--- FiberDeskApp/app/build.gradle.kts
+++ FiberDeskApp/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-parcelize")
 }
 
 android {
@@ -46,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
 }
---
+++ FiberDeskApp/app/src/main/java/com/example/fiberdesk_app/TicketService.kt
@@ -0,0 +1,34 @@
+package com.example.fiberdesk_app
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TicketService {
+
+    // Crear ticket
+    @POST("api/tickets")
+    fun crearTicket(
+        @Body ticket: Ticket
+    ): Call<Map<String, Any>>
+
+    // Listar tickets
+    @GET("api/tickets")
+    fun getTickets(): Call<List<Ticket>>
+
+    // Buscar tickets
+    @GET("api/tickets/search")
+    fun searchTickets(
+        @Query("query") query: String
+    ): Call<List<Ticket>>
+
+    // Archivar ticket
+    @PUT("api/tickets/archive/{folio}")
+    fun archivarTicket(
+        @Path("folio") folio: String
+    ): Call<Map<String, Any>>
+}

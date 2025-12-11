0179892b4951ebd95a8c66c5ce5c397c6c308e20
Roberto Negrete <notlurk64@gmail.com>
2025-12-10 21:41:43 -0600

    rediseño del modelo del ticket

--- FiberDeskApp/app/src/main/java/com/example/fiberdesk_app/Ticket.kt
+++ FiberDeskApp/app/src/main/java/com/example/fiberdesk_app/Ticket.kt
@@ -1,24 +1,15 @@
 package com.example.fiberdesk_app
 
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-
-@Parcelize
 data class Ticket(
-
-    val id: String,
-
-
     val folio: String,
     val cliente: String,
     val prioridad: String,
-
-
     val asunto: String,
     val tecnico: String,
     val creadoPor: String,
     val estado: String,
-    val fechaCreacion: String
-) : Parcelable
+    val fecha: String,
+    val descripcion: String,
+    val archivado: Boolean = false
+)
